5438235ec70a8c7bb3d9c119669cbd4cbd76ddf6
cezarlofellem@gmail.com <cezarlofellem@gmail.com>
2023-08-09 11:26:23 -0400

    updated js + initiated proj FB UI

--- index.js
+++
@@ -1,7 +0,0 @@
-const btnGenerate = document.getElementById("btn");
-
-function getGenerate() {
-  console.log("Generate");
-}
-
-btnGenerate.addEventListener("click", getGenerate);
---
+++ project-dailydosegenerator/index.js
@@ -0,0 +1,5 @@
+document.querySelector("#btnGenerate").addEventListener("click", btnGenerate);
+
+function btnGenerate() {
+  console.log("Clicked");
+}
